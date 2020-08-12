8cf0b23f631d8a7240fab3e386c39df4895b296b
RI2N <akhmadriduan66@gmail.com>
2020-08-13 03:17:43 +0700

    tambahan gambar

---
+++ app/src/main/java/com/gurumalas/ulamakalsel/DataUlamaBanjar.java
@@ -0,0 +1,42 @@
+package com.gurumalas.ulamakalsel;
+com.dicoding.picodiploma.myrecylerview.model
+
+class DataUlamaBanjar {
+    private static String[] NamaUlamaBanjar = {
+            "Syekh Muhammad Arsyad Al Banjari", //1
+            "Syekh Muhammad Zaini bin Abdul Ghani", //2
+            "Syekh Abdussamad", //3
+            "Syekh Muhammad Nafis Al Banjari", //4
+            "Syekh Abdul Hamid Abulung", //5
+            "Syekh Jamaluddin Surgi Mufti", //6
+            "Habib Hamid bin Abbas Bahasyim", //7
+            "Syekh Abdurrahman Siddiq", //8
+            "Tuan Guru Zainal Ilmi Dalam Pagar", //9
+            "Tuan Guru Kasyful Anwar" //10
+    };
+
+    private static String[] Detailulama = {
+            "ulama banjar.", //1
+            "ulama banjar.", //2
+            "ulama banjar.", //3
+            "ulama banjar.", //4
+            "ulama banjar.", //5
+            "ulama banjar.", //6
+            "ulama banjar.", //7
+            "ulama banjar.", //8
+            "ulama banjar.", //9
+            "ulama banjarr." //10
+    };
+    private static int[] heroesImages = {
+            R.drawable.syekhmuhammadarsyadalbanjari,
+            R.drawable.gurusekumpul,
+            R.drawable.syekhabdussamad,
+            R.drawable.syekhmuhammadnafisalbanjari,
+            R.drawable.syekhabdulhamidabulung,
+            R.drawable.syekhjamaluddinsurgimufti,
+            R.drawable.habibhamidbinabbasbahasyim,
+            R.drawable.syekhabudurahmansiddiq,
+            R.drawable.tuanguruzainalilmidalampagar,
+            R.drawable.khkasyfulanwar
+    };
+}
